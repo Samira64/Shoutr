c644f84be599fdb63a6f75d52186d79d212a9325
samira <s.soltani336@gmail.com>
2017-09-07 01:11:35 +0800

    capture username

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,7 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
+  Rails.configuration.action_controller.per_form_csrf_tokens = true
+
 end
