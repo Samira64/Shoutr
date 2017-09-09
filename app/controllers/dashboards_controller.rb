19d2b69ed495e8a514ae629a200260dfafdd42ab
samira <s.soltani336@gmail.com>
2017-09-09 21:23:00 +0800

    allow user to shout

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
-	def show
-		render
-	end
+    def show
+        @shout = Shout.new
+        @shouts = current_user.shouts
+        render
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   constraints Clearance::Constraints::SignedIn.new do
     root to: "dashboards#show"
   end
-  
+
   root to: "homes#show" 
+  resources :shouts, only: [:create]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
   resources :users, only: [:create] do
