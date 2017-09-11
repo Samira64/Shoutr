2de5654c1c70ecb28af77c00f120166a11da4b4f
samira <s.soltani336@gmail.com>
2017-09-12 01:05:57 +0800

    Refactor application to be polymorphic

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -9,8 +9,17 @@ class ShoutsController < ApplicationController
     end
 
     private
+
     def shout_params
-        params.require(:shout).permit(:body)
+        { content: content_from_params }
+    end
+
+    def content_from_params
+        TextShout.new(content_params)
+    end
+    
+    def content_params
+        params.require(:shout).require(:content).permit(:body)
     end
 
     def redirect_options_for(shout)
