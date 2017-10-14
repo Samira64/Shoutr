63332f0bea9cefd6b411f5d185813abb82712b91
samira <s.soltani336@gmail.com>
2017-10-14 14:49:50 +0800

    Add followers relationship

--- db/schema.rb
+++ db/schema.rb
@@ -62,15 +62,15 @@ ActiveRecord::Schema.define(version: 20171011110829) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.string   "email",                            null: false
-    t.string   "encrypted_password",   limit: 128, null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "email",                                        null: false
+    t.string   "encrypted_password",   limit: 128,             null: false
     t.string   "confirmation_token",   limit: 128
-    t.string   "remember_token",       limit: 128, null: false
+    t.string   "remember_token",       limit: 128,             null: false
     t.string   "username"
-    t.integer  "followed_users_count"
-    t.integer  "followers_count"
+    t.integer  "followed_users_count",             default: 0, null: false
+    t.integer  "followers_count",                  default: 0, null: false
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
