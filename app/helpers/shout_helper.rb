42e224a4bf903b0891e59eead5ffe92e3ece39be
samira <s.soltani336@gmail.com>
2017-10-18 19:16:18 +0800

    Refactor shout forms

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,13 +1,21 @@
 module ShoutHelper
-  def like_button(shout)
-  	if current_user.liked?(shout)
-  		link_to "Unlike", unlike_shout_path(shout), method: :delete
-  	else
-  		link_to "Like", like_shout_path(shout), method: :post
-  	end
-  end
+  def shout_form_for(content_type)
+    form_for Shout.new do |form| 
+     form.hidden_field(:content_type, value: content_type) +
+     form.fields_for(:content) { |content_form| yield(content_form) } +
+     form.submit("Shout!")
+   end 
+ end
 
-  def autolink(text)
-    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
+ def like_button(shout)
+   if current_user.liked?(shout)
+    link_to "Unlike", unlike_shout_path(shout), method: :delete
+  else
+    link_to "Like", like_shout_path(shout), method: :post
   end
+end
+
+def autolink(text)
+  text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
+end
 end
